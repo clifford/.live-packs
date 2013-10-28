11fb4dd3adc5bda8236898914a4ec0d57630590b
clifford goldberg <clifford.goldberg@gmail.com>
2013-10-28 21:22:04 +0200

    dot-mode added as standalone git submodule

--- .emacs-live.el
+++ .emacs-live.el
@@ -1,3 +1,5 @@
 (live-ignore-packs '(live/colour-pack))
 ;; add my own pack which sets most of the bindings-pack with a few exclusions
-(live-append-packs '(~/.live-packs/cig-pack))
+(live-append-packs '(~/.live-packs/cig-pack
+                     ~/.live-packs/solarized-pack
+                     ~/.live-packs/dot-mode))
--- cig-pack/init.el
+++ cig-pack/init.el
@@ -5,10 +5,3 @@
 
 ;; Load bindings config
 (live-load-config-file "bindings.el")
-
-;; load dot-mode File
-;; see: http://wyrick.org/source/elisp/dot-mode/
-;;      https://github.com/wyrickre/dot-mode
-(live-add-pack-lib "dot-mode")
-(require 'dot-mode)
-(add-hook 'find-file-hooks 'dot-mode-on)
