50c5d6d0ceff4ec7c90fff1a2f60839231b5bf84
clifford goldberg <clifford.goldberg@gmail.com>
2014-02-04 17:56:11 +0200

    use emacs-live dev packs

--- .emacs-live.el
+++ .emacs-live.el
@@ -2,3 +2,6 @@
 
 ;; add my own pack which sets most of the bindings-pack with a few exclusions
 (live-append-packs '(~/.live-packs/cig-pack))
+
+;; make emacs-live use the dev packs (still in testing)
+(live-use-dev-packs)
