aa8dbf0741672bbdf30daf50f0de6040d1fa8cf3
clifford goldberg <clifford.goldberg@gmail.com>
2014-02-06 21:14:12 +0200

    disabling the default colour pack got solarized working. also uncommented the dev packs

--- .emacs-live.el
+++ .emacs-live.el
@@ -1,7 +1,13 @@
+(live-use-packs '(live/foundation-pack
+                 ;live/colour-pack ;disabled for solarized to work properly
+                 live/clojure-pack
+                 live/lang-pack
+                 live/power-pack))
+
 (live-add-packs '(~/.live-packs/solarized-pack))
 
 ;; add my own pack which sets most of the bindings-pack with a few exclusions
 (live-append-packs '(~/.live-packs/cig-pack))
 
 ;; make emacs-live use the dev packs (still in testing)
-;;(live-use-dev-packs)
+(live-use-dev-packs)
