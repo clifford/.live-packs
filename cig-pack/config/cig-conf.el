681d3764ed790125fb1a0bd57d83101389748f25
clifford goldberg <clifford.goldberg@gmail.com>
2015-01-07 13:09:10 +0200

    reduced size of font from 12 to 11

--- cig-pack/config/cig-conf.el
+++ cig-pack/config/cig-conf.el
@@ -18,7 +18,7 @@
 ;; Custom font, trick from https://github.com/overtone/emacs-live/issues/25
 
 ;; (add-to-list 'default-frame-alist '(font . "Inconsolata 12"))
-(add-to-list 'default-frame-alist '(font . "Anonymous Pro 12"))
+(add-to-list 'default-frame-alist '(font . "Anonymous Pro 11"))
 ;;(add-to-list 'default-frame-alist '(font . "PragmataPro 10"))
 ;;(add-to-list 'default-frame-alist '(font . "Consolas 11"))
 
